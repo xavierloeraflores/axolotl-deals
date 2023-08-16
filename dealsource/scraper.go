970237edb021d41a6a023c315197fc53286ba5b3
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-08-16 10:21:06 -0700

    Quick save

--- dealsource/scraper.go
+++ dealsource/scraper.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"time"
+
 	"github.com/gocolly/colly"
 )
 
@@ -38,14 +40,56 @@ func getItems() []DealSourcedItem {
 	return items
 }
 
+func getAmazonLinkByTinyURL(tinyURL string)string{
+	var amazonLink string
+	println("tinyURL", tinyURL)
+	c := colly.NewCollector(
+		colly.AllowedDomains("tinyurl.com", "www.amazon.com", "amazon.com", "*", "www.tinyurl.com"),
+		colly.Async(false),
+		colly.MaxDepth(10),
+	)
+	// c.Limit(&colly.LimitRule{
+	// 	DomainGlob:  "*",
+	// 	Parallelism: 2,
+	// 	RandomDelay: 	 3*time.Second,
+	// })
+	c.SetRequestTimeout(5*time.Second)
+
+	c.OnRequest(func(r *colly.Request) {
+		println("Requested Visiting", r.URL.String())
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		println("Response Visited", r)
+	})
+
+	c.OnHTML("a[href]" , func(e *colly.HTMLElement) {
+		// amazonLink = e.ChildAttr("#redirect-url", "value")
+		println("HTML----",e)
+	})
+
+	c.OnXML("a[href]" , func(x *colly.XMLElement)  {
+		// amazonLink = e.ChildAttr("#redirect-url", "value")
+		println("XML----",x)
+	})
+	c.OnError(func(r *colly.Response, err error) {
+		println("Error:", r.StatusCode, err)
+	})
+	c.Visit(tinyURL)
+	// c.Request("GET", tinyURL, nil, nil, nil)
+	c.Wait()
+	println("xxx", amazonLink)
+	return amazonLink
+}
 
 
 func main() {
 	var dealSourcedItems []DealSourcedItem
 	dealSourcedItems = getItems()
 
-	for i := 0;  i< len(dealSourcedItems); i++ {
+	for i := 0;  i< 1; i++ {
 		println(i, dealSourcedItems[i].Title)
+		getAmazonLinkByTinyURL(dealSourcedItems[i].Link)
 	}
 
 
