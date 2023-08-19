40c9327bd8cbaa32dfdfd77afee02db3dd7823d9
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-08-19 07:35:46 -0700

    FEAT: Next.js client

---
+++ client/src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { type AppType } from "next/app";
+import { api } from "npm/utils/api";
+import "npm/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default api.withTRPC(MyApp);
