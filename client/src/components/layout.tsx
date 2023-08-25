2b64975b14b0d076148235b011ee9641cfe96c1c
Xavier Loera Flores <x.loera.tech@gmail.com>
2023-08-25 16:09:40 -0700

    FEAT: <main> Layout

--- client/src/components/layout.tsx
+++ client/src/components/layout.tsx
@@ -12,7 +12,9 @@ export default function RootLayout({
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Toaster />
       <Header />
-      {children}
+      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+        {children}
+      </main>
       <Footer />
     </ThemeProvider>
   );
